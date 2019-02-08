5995d31a5e81b975aed053052e3a779efb381b74
Kalle Johansson <kajoh@raa.se>
2019-02-08 10:42:41 +0100

    First commit

---
+++ src/main/java/se/raa/cdraa/CdraaApplication.java
@@ -0,0 +1,14 @@
+package se.raa.cdraa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CdraaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CdraaApplication.class, args);
+	}
+
+}
+
---
+++ src/test/java/se/raa/cdraa/CdraaApplicationTests.java
@@ -0,0 +1,17 @@
+package se.raa.cdraa;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class CdraaApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
+
